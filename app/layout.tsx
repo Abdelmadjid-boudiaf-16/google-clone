a5978d2f3ec78d85fbb8fe760b374e2a536d4bd3
Abdelmadjid-boudiaf-16 <boudiafabdelmadjid16@gmail.com>
2024-09-28 18:30:20 +0100

    add metadata description

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils";
 const prompt = Prompt({ subsets: ["latin"], weight: "500" });
 export const metadata: Metadata = {
   title: "Google clone app",
-  description: "",
+  description: "Google Clone using Next.js, Tailwind CSS, and Shadcn UI – A modern web application that replicates the Google search experience, utilizing Next.js 14, Tailwind CSS for responsive design, and Shadcn UI for sleek components. This project demonstrates the use of dynamic data fetching and interactive UI.",
 };
 
 export default function RootLayout({
